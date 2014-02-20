86d7b55d91b5c753f2db9a5efdf6a3bbac350100
Allen Short <ashort@mozilla.com>
2014-02-20 15:27:38 -0600

    import from zamboni, up to c5aa1e419d29cdd59730d2bce30fed40c99daec4

--- apps/devhub/tests/test_views.py
+++ apps/devhub/tests/test_views.py
@@ -6,10 +6,8 @@ from datetime import datetime, timedelta
 from decimal import Decimal
 
 from django.conf import settings
-from django.core import mail
 from django.core.files.storage import default_storage as storage
 
-import jingo
 import mock
 import waffle
 from jingo.helpers import datetime as datetime_filter
@@ -26,7 +24,7 @@ import amo
 import amo.tests
 import files
 import paypal
-from addons.models import Addon, AddonCategory, AddonUpsell, Category, Charity
+from addons.models import Addon, AddonCategory, Category, Charity
 from amo.helpers import (absolutify, babel_datetime, url as url_reverse,
                          timesince)
 from amo.tests import (addon_factory, assert_no_validation_errors, formset,
@@ -440,44 +438,6 @@ class TestEditPayments(amo.tests.TestCase):
         self.check(paypal_id='', suggested_amount=Decimal('11.50'),
                    charity=Charity.objects.get(name='fligtar fund'))
 
-    @mock.patch('devhub.views.client')
-    def test_success_solitude(self, client):
-        self.create_flag(name='solitude-payments')
-        self.post(recipient='dev', suggested_amount=2, paypal_id='greed@dev',
-                  annoying=amo.CONTRIB_AFTER)
-        eq_(client.create_seller_paypal.call_args[0][0], self.addon)
-        eq_(client.patch_seller_paypal.call_args[1]['data'],
-            {'paypal_id': u'greed@dev'})
-
-    @mock.patch('devhub.views.client')
-    def test_success_solitude_charity(self, client):
-        self.create_flag(name='solitude-payments')
-        d = dict(recipient='org', suggested_amount=11.5,
-                 annoying=amo.CONTRIB_PASSIVE)
-        d.update({'charity-name': 'fligtar fund',
-                  'charity-url': 'http://feed.me',
-                  'charity-paypal': 'greed@org'})
-        self.post(d)
-        eq_(client.create_seller_paypal.call_args[0][0], self.addon)
-        eq_(client.patch_seller_paypal.call_args[1]['data'],
-            {'paypal_id': u'greed@org'})
-
-    @mock.patch('devhub.views.client')
-    def test_uses_solitude(self, client):
-        self.create_flag(name='solitude-payments')
-        self.addon.update(charity=self.foundation)
-        client.get_seller_paypal_if_exists.return_value = {'paypal_id': 'f@f'}
-        res = self.client.get(self.url)
-        eq_(res.context['addon'].paypal_id, 'f@f')
-        eq_(res.context['contrib_form'].initial['paypal_id'], 'f@f')
-
-    @mock.patch('devhub.views.client')
-    def test_uses_solitude_missing(self, client):
-        self.create_flag(name='solitude-payments')
-        client.get_seller_paypal_if_exists.return_value = None
-        res = self.client.get(self.url)
-        eq_(res.context['addon'].paypal_id, '')
-
     def test_dev_paypal_id_length(self):
         r = self.client.get(self.url)
         doc = pq(r.content)
@@ -792,12 +752,6 @@ class TestPaymentsProfile(amo.tests.TestCase):
 
         eq_(result[u'valid'], False)
         assert len(result[u'message']) > 0, "No error on missing paykey"
-
-    @mock.patch('paypal.get_paykey')
-    def test_checker_no_pre_approval(self, get_paykey):
-        self.client.post(reverse('devhub.check_paypal'),
-                         {'email': 'test@test.com'})
-        assert 'preapprovalKey' not in get_paykey.call_args[0][0]
 
 
 class TestDelete(amo.tests.TestCase):
@@ -1310,7 +1264,7 @@ class TestSubmitStep3(TestSubmitBase):
     def test_submit_categories_remove(self):
         c = Category.objects.get(id=23)
         AddonCategory(addon=self.addon, category=c).save()
-        eq_([c.id for c in self.get_addon().all_categories], [22, 23])
+        eq_([a.id for a in self.get_addon().all_categories], [22, 23])
 
         self.cat_initial['categories'] = [22]
         self.client.post(self.url, self.get_dict(cat_initial=self.cat_initial))
@@ -2651,7 +2605,7 @@ class TestRequestReview(amo.tests.TestCase):
     fixtures = ['base/users', 'base/platforms']
 
     def setUp(self):
-        self.addon = Addon.objects.create(type=amo.ADDON_EXTENSION, name='xxx')
+        self.addon = Addon.objects.create(type=1, name='xxx')
         self.version = Version.objects.create(addon=self.addon)
         self.file = File.objects.create(version=self.version,
                                         platform_id=amo.PLATFORM_ALL.id)
@@ -2748,34 +2702,6 @@ class TestRequestReview(amo.tests.TestCase):
         self.addon.update(status=amo.STATUS_LITE_AND_NOMINATED)
         eq_(self.get_version().nomination.timetuple()[0:5],
             orig_date.timetuple()[0:5])
-
-
-class TestDeleteVersion(amo.tests.TestCase):
-    fixtures = ['base/users', 'base/platforms']
-
-    def setUp(self):
-        self.addon = Addon.objects.create(type=amo.ADDON_EXTENSION, name='xxx')
-        self.version = Version.objects.create(addon=self.addon)
-        self.file = File.objects.create(version=self.version,
-                                        platform_id=amo.PLATFORM_ALL.id,
-                                        status=amo.STATUS_PUBLIC)
-        self.url = reverse('devhub.versions.delete', args=[self.addon.slug])
-        self.login('admin@mozilla.com')
-
-    def test_delete_version(self):
-        self.client.post(self.url, {'version_id': self.version.id,
-                                    'addon_id': self.addon.id})
-
-        assert not Version.objects.filter(id=self.version.id).exists()
-
-    def test_disable_version(self):
-        self.client.post(self.url, {'version_id': self.version.id,
-                                    'addon_id': self.addon.id,
-                                    'disable_version': ''})
-
-        assert Version.objects.filter(id=self.version.id).exists()
-        self.file.reload()
-        eq_(self.file.status, amo.STATUS_DISABLED)
 
 
 class TestRedirects(amo.tests.TestCase):
